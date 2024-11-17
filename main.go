4b5d8e6aa15874cbd52aa4db64aba8e18679ada5
Guy Sobol <g.sobol@f5.com>
2024-11-17 15:34:42 +0200

    Init empty

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	api "f5.com/ha/pkg"
-)
-
 func main() {
-	http.HandleFunc("/register", api.Register)
-	http.HandleFunc("/login", api.Login)
-	http.HandleFunc("/accounts", api.Auth(api.AccountsHandler))
-	http.HandleFunc("/balance", api.Auth(api.BalanceHandler))
 
-	fmt.Println("Server starting on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }

dd00af2998334c9eb03f1403e66fac46f830a4ad
timkush1 <127944880+timkush1@users.noreply.github.com>
2024-11-25 20:10:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"f5.com/ha/pkg"
+)
+
+func main() {
+	// Public routes without authentication
+	http.HandleFunc("/register", pkg.Register)
+	http.HandleFunc("/login", pkg.Login)
+
+	// applying middleware to routes
+	http.HandleFunc("/accounts", pkg.RoleMiddleware("admin", pkg.AccountsHandler))
+	http.HandleFunc("/balance",  pkg.Auth(pkg.BalanceHandler))             
+
+	port := ":8080"
+	log.Printf("Starting server on port %s...", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
